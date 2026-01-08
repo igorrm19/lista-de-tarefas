7d0f0703abd8d3c552b32c01794cd7c9329d9170
igorrm23 <igormachadofire14@gmail.com>
2026-01-08 02:19:14 -0300

    fix: replace deprecated substr with slice method

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function saveTasks() {
 }
 
 function generateId() {
-    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+    return Date.now().toString(36) + Math.random().toString(36).slice(2);
 }
 
 function renderTasks() {
